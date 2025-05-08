0de76372c94b9beb9873a67c401acdedcf15c3df
Tantanok221 <6572withspicy@gmail.com>
2025-05-08 11:03:24 +0800

    feat: added search bar at banner with basic ui

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,12 @@
 class PageController < ApplicationController
   def index
+    @albums = Album.with_associations
+
+  end
+
+  def search
+    @albums = Album.with_associations
+                   .filter_by_names_with(params[:search_by_name])
   end
 
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,17 @@
 class Album < ApplicationRecord
   has_many :songs, dependent: :destroy
-  
+  has_many :album_artist_mappings, dependent: :destroy
+  has_many :artists, through: :album_artist_mappings
+
+  has_many :album_genre_mappings, dependent: :destroy
+  has_many :genres, through: :album_genre_mappings
+
+  def self.filter_by_names_with(name)
+    where("name like ?", "#{name}%")
+  end
+
+  scope :with_associations, -> {
+    includes(:album_artist_mappings, :artists, :album_genre_mappings, :genres)
+  }
+
 end
