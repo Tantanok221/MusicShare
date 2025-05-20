ebd7d45b6253c0c542bf14b4781fee95a9690bfd
Tantanok221 <6572withspicy@gmail.com>
2025-05-20 23:01:38 +0800

    feat: add to list function now works

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -13,9 +13,15 @@ class PlaylistController < ApplicationController
   end
 
   def add_album_to_playlist
-    @playlist = Playlist.find(params[:id])
     @album = Album.find(params[:album_id])
-    @playlist.songs << @album.songs
+
+    # Loop through each selected playlist ID
+    params[:playlist_ids]&.each do |playlist_id|
+      playlist = current_user.playlists.find(playlist_id)
+      playlist.songs << @album.songs
+    end
+
+    redirect_back(fallback_location: root_path, notice: "Album added to playlists")
   end
 
   def add_song_to_playlist
