bed91162220d84db9e7b87d68be4dcd5b898e7bc
Tantanok221 <6572withspicy@gmail.com>
2025-05-21 14:51:38 +0800

    feat: added working search bar in the admin home page and ui
    (cherry picked from commit a07a545fa74563fad01dee84ce0682fc39bd10be)

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 
 Rails.application.routes.draw do
-  get "admin/home"
   devise_for :users, controllers: { registrations: "users/registrations" }
   get "page/index"
   root "page#index"
   get "search", to: "page#search", as: :search
   get "playlist/:id", to: "page#list", as: :list
-  get "album/:id", to: "page#album_details", as: :album_details
   get "playlist", to: "page#playlist_index", as: :list_index
+  get "album/:id", to: "page#album_details", as: :album_details
   get "profile/:username", to: "page#profile", as: :profile
   # Playlist routes
   post "create_playlist", to: "playlist#create", as: :create_playlist
   post "add_album_to_playlist", to: "playlist#add_album_to_playlist", as: :add_album_to_playlist
   post "add_song_to_playlist", to: "playlist#add_song_to_playlist", as: :add_song_to_playlist
+
+  # Admin routes
+  get "admin", to: "admin#home", as: :admin_home
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
