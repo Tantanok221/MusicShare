bd35c935932de6734e58eab20de6bb4db6790e47
tonju <tonju112420@gmail.com>
2025-05-22 14:01:36 +0800

    feat:fix conflict

--- app/components/search_box_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class SearchBoxComponent < ViewComponent::Base
-    def initialize(form_path:, search_param: :search_by_name)
-        @form_path = form_path
-        @search_param = search_param
-    end
-end
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,9 @@
 class AdminController < ApplicationController
   def home
+    if params[:search_by_name].present?
+      @artists = Artist.where("name LIKE ?", "%#{params[:search_by_name]}%")
+    else
+      @artists = Artist.all
+    end
   end
 end
