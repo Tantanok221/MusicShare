c1d96ab4dae628bd5bbbaee35fe3e4dd3e5ecd98
Tantanok221 <6572withspicy@gmail.com>
2025-05-23 23:38:17 +0800

    feat: added destroy playlist button

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,7 +1,7 @@
 class Playlist < ApplicationRecord
   belongs_to :user
 
-  has_many :playlist_song_mappings
+  has_many :playlist_song_mappings, dependent: :destroy
   has_many :songs, through: :playlist_song_mappings
 
   def self.filter_by_id(id)
